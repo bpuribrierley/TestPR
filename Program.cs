00632e5e808a5a3bbaa79da08a55fefcf814741a
bpuribrierley <113200794+bpuribrierley@users.noreply.github.com>
2022-11-29 14:30:24 -0600

    Typo fix

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace TestPR
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!")
+            Console.WriteLine("Hello World!");
         }
     }
 }
